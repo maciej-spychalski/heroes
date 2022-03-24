7ababba74d6bd9ef8661b5c5593d5b5348b1f1f3
maciej.spychalski <maciej.spychalski@gmail.com>
2022-03-24 12:13:30 +0100

    Fixed problem with exporting svg files

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,5 @@
-export const combat = require('./boxing.svg');
-export const strength = require('./fist.svg');
-export const speed = require('./speedometer.svg');
-export const intelligence = require('./thinking.svg');
-export const durability = require('./durable.svg');
+export const combat = require('./boxing.svg').default;
+export const strength = require('./fist.svg').default;
+export const speed = require('./speedometer.svg').default;
+export const intelligence = require('./thinking.svg').default;
+export const durability = require('./durable.svg').default;
