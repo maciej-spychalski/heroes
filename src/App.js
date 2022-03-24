cfb3fa202d82bcb9603d99a74f0d84708c4eed1e
maciej.spychalski <maciej.spychalski@gmail.com>
2022-03-24 10:25:37 +0100

    Added react router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React from 'react';
 import './App.css';
 import Nav from './Components/Nav/Nav';
 import HeroesFeatured from './Components/HeroesFeatured/HeroesFeatured';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Nav></Nav>
-      <main>
-        <div className='container'>
-          <HeroesFeatured />
-        </div>
-      </main>
-      <footer></footer>
+      <Router>
+        <Nav />
+        <main>
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<HeroesFeatured />} />
+              {/* <Route path='/search/:name' element={} /> */}
+
+              {/* <Route path='/hero/:id' element={} /> */}
+            </Routes>
+          </div>
+        </main>
+        <footer>
+          <div className="container">
+            <p>This content is kindly provided by <a href="https://superheroapi.com/">Superhero API</a></p>
+          </div>
+        </footer>
+      </Router>
     </>
   );
 
